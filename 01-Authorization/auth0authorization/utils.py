a3e3182aff34ec21e1431d9e0cbdc2468190f7c0
Maylon Pedroso <mpedroso@stackbuilders.com>
2021-08-17 11:13:44 -0500

    Update python dependencies and simplify some of the logic: - Use drf Response class for views responses - Get token from the request on scope validation - Use PyJWTClient to get the token validation public key

--- 01-Authorization/auth0authorization/utils.py
+++ 01-Authorization/auth0authorization/utils.py
@@ -1,9 +1,10 @@
-import json
-import os
+from urllib.error import URLError
+
+import jwt
 
 from django.contrib.auth import authenticate
-import jwt
-import requests
+from django.conf import settings
+from rest_framework.exceptions import AuthenticationFailed
 
 
 def jwt_get_username_from_payload_handler(payload):
@@ -14,16 +15,12 @@ def jwt_get_username_from_payload_handler(payload):
 
 def jwt_decode_token(token):
     header = jwt.get_unverified_header(token)
-    auth0_domain = os.environ.get('AUTH0_DOMAIN')
-    jwks = requests.get('https://{}/.well-known/jwks.json'.format(auth0_domain)).json()
-    public_key = None
-    for jwk in jwks['keys']:
-        if jwk['kid'] == header['kid']:
-            public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+    key_client = jwt.PyJWKClient('https://{}/.well-known/jwks.json'.format(settings.AUTH0_DOMAIN))
+    try:
+        public_key = key_client.get_signing_key(header['kid']).key
+    except (jwt.PyJWKClientError, URLError):
+        raise AuthenticationFailed('Public key not found.')
 
-    if public_key is None:
-        raise Exception('Public key not found.')
-
-    api_identifier = os.environ.get('API_IDENTIFIER')
-    issuer = 'https://{}/'.format(auth0_domain)
+    api_identifier = settings.API_IDENTIFIER
+    issuer = settings.JWT_ISSUER
     return jwt.decode(token, public_key, audience=api_identifier, issuer=issuer, algorithms=['RS256'])
